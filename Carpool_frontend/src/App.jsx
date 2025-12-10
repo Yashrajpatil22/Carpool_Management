d5cf75f7d0cf151c58cde9c35f36f8b30918f49a
AaryaKhatate <aarya16.khatate06@gmail.com>
2025-12-10 11:52:50 +0530

    feat: add Live Tracking page in UI

--- Carpool_frontend/src/App.jsx
+++ Carpool_frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Notifications from './pages/Notifications';
 import Schedules from './pages/Schedules';
 import MyCarpools from './pages/MyCarpools';
 import SuggestedRides from './pages/SuggestedRides';
+import LiveTracking from './pages/LiveTracking';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/schedules" element={<Schedules />} />
         <Route path="/carpools" element={<MyCarpools />} />
         <Route path="/suggested-rides" element={<SuggestedRides />} />
+        <Route path="/tracking" element={<LiveTracking />} />
       </Routes>
     </Router>
   );
