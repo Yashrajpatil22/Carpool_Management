488962448173f61384a948a9d3db98253b7d29f4
Yashraj Patil <yashrajpatil2206@gmail.com>
2025-12-11 09:26:26 +0530

    API: /rideTracking

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,12 +10,12 @@ import routePointRouter from "./routes/routepointsrouter.js";
 import rideRequestRouter from "./routes/rideRequestRouter.js";
 import rideAssignmentRouter from "./routes/rideAssignmentRouter.js";
 import rideSuggestionRouter from "./routes/rideSuggestionRouter.js";
+import rideTrackingRouter from "./routes/rideTrackingRouter.js";
 
 dotenv.config();
 
 const app = express();
 
-// CORS middleware
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -37,6 +37,7 @@ app.use("/api/routepoints", routePointRouter);
 app.use("/api/riderequest", rideRequestRouter);
 app.use("/api/rideassignment", rideAssignmentRouter)
 app.use("/api/ridesuggestion", rideSuggestionRouter);
+app.use("/api/ridetracking", rideTrackingRouter);
 
 
 connectDB().then(() => {
