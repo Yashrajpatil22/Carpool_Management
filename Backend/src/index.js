4c540d18cd701d082ba9518cc10d8022257e0e5b
Yashraj Patil <yashrajpatil2206@gmail.com>
2025-12-11 12:00:51 +0530

    cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -21,7 +21,11 @@ dotenv.config();
 const app = express();
 
 app.use((req, res, next) => {
-  const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://carpool-management-3.onrender.com/",
+  ];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
